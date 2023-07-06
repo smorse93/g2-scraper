f6c64b2ca50292f692a5aed89753b9bb170e8d8e
Chetan11-Dev <chetansirsa@gmail.com>
2023-07-07 01:31:36 +0530

    fixes

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,6 @@ from .extract_product_links import ExtractProductLinks
 from .extract_products import ExtractProducts
 
 tasks_to_be_run = [
-        # ExtractProductLinks,
-        ExtractProducts
+        ExtractProductLinks,
+        # ExtractProducts
 ]
